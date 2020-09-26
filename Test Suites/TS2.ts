8207a004edf823049575d563dd49d6c377e14c69
MAY <MAY@MAY20137>
2020-09-26 16:47:11 +0700

    :D

---
+++ Test Suites/TS2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0f1a297c-2ec5-4466-b5cd-5fd4269939d5</testSuiteGuid>
+</TestSuiteEntity>
